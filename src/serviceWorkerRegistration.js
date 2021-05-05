721b124f2d746cbbe39c21d1a962871f0488e9c2
Anne Denicourt <annedenicourt@gmail.com>
2021-05-05 10:28:25 +0200

    config PWA

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -1,12 +1,3 @@
-// This optional code is used to register a service worker.
-// register() is not called by default.
-
-// This lets the app load faster on subsequent visits in production, and gives
-// it offline capabilities. However, it also means that developers (and users)
-// will only see deployed updates on subsequent visits to a page, after all the
-// existing tabs open on the page have been closed, since previously cached
-// resources are updated in the background.
-
 // To learn more about the benefits of this model and instructions on how to
 // opt-in, read https://cra.link/PWA
 
@@ -33,11 +24,7 @@ export function register(config) {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
-        // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
-
-        // Add some additional logging to localhost, pointing developers to the
-        // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
           console.log(
             'This web app is being served cache-first by a service ' +
@@ -45,7 +32,6 @@ export function register(config) {
           );
         });
       } else {
-        // Is not localhost. Just register service worker
         registerValidSW(swUrl, config);
       }
     });
